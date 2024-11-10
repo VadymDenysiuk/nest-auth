f6219f7ffbdc2fd99e032d4cd142632c1c67205c
VadymDenysiuk <80970410371q@gmail.com>
2024-11-10 16:39:32 +0200

    add userResponse

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import {
     BadRequestException,
     Body,
+    ClassSerializerInterceptor,
     Controller,
     Get,
     HttpStatus,
     Post,
     Res,
     UnauthorizedException,
+    UseInterceptors,
 } from '@nestjs/common';
 import { LoginDto, RegisterDto } from './dto';
 import { AuthService } from './auth.service';
@@ -14,6 +16,7 @@ import { Tokens } from './interfaces';
 import { Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { Cookie, Public, UserAgent } from '@common/decorators';
+import { UserResponse } from 'src/user/responses';
 
 const REFRESH_TOKEN = 'refreshtoken';
 
@@ -25,6 +28,7 @@ export class AuthController {
         private readonly configService: ConfigService,
     ) {}
 
+    @UseInterceptors(ClassSerializerInterceptor)
     @Post('register')
     async register(@Body() dto: RegisterDto) {
         const user = await this.authService.register(dto);
@@ -34,7 +38,7 @@ export class AuthController {
                 `it is not possible to register a user with data ${JSON.stringify(dto)}`,
             );
 
-        return user;
+        return new UserResponse(user);
     }
 
     @Post('login')
