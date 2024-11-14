a19da0089921351273174b868fef45c146869e22
VadymDenysiuk <80970410371q@gmail.com>
2024-11-14 18:10:10 +0200

    add update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
 import {
+    Body,
     ClassSerializerInterceptor,
     Controller,
     Delete,
     Get,
     Param,
     ParseUUIDPipe,
+    Put,
     UseGuards,
     UseInterceptors,
 } from '@nestjs/common';
@@ -13,7 +15,7 @@ import { UserResponse } from './responses';
 import { CurrentUser, Roles } from '@common/decorators';
 import { JwtPayload } from '@auth/interfaces';
 import { RolesGuard } from '@auth/guards/role.guard';
-import { Role } from '@prisma/client';
+import { Role, User } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
@@ -37,5 +39,13 @@ export class UserController {
     @Get()
     me(@CurrentUser() user: JwtPayload) {
         return user;
+    }
+
+    @UseInterceptors(ClassSerializerInterceptor)
+    @Put()
+    async updateUser(@Body() body: Partial<User>) {
+        const user = await this.userService.save(body);
+
+        return new UserResponse(user);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -19,12 +19,21 @@ export class UserService {
     async save(user: Partial<User>) {
         const hashedPassword = user.password ? this.hashPassword(user.password) : null;
 
-        const savedUser = await this.prismaService.user.create({
-            data: {
+        const savedUser = await this.prismaService.user.upsert({
+            where: {
+                email: user.email,
+            },
+            update: {
+                password: hashedPassword ?? undefined,
+                provider: user?.provider ?? undefined,
+                roles: user?.roles ?? undefined,
+                isBlocked: user?.isBlocked ?? undefined,
+            },
+            create: {
                 email: user.email,
                 password: hashedPassword,
-                roles: ['USER'],
                 provider: user?.provider,
+                roles: ['USER'],
             },
         });
 
